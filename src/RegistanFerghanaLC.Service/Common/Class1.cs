5e17169d6be10ee34e1e949991c80e53005a3893
O'tkirbek <O'tkirbek@DESKTOP-Q53DFVB>
2023-02-23 18:18:41 +0500

    service layer architected

---
+++ src/RegistanFerghanaLC.Service/Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistanFerghanaLC.Service.Common
+{
+    internal class Class1
+    {
+    }
+}
