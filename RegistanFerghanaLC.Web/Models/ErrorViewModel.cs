fa92a8eb0f63b38653db857f300f2982e4507113
O'tkirbek <O'tkirbek@DESKTOP-Q53DFVB>
2023-02-23 17:59:23 +0500

    created mvc project

---
+++ RegistanFerghanaLC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RegistanFerghanaLC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
