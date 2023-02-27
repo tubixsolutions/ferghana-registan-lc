845d52358c6ff889c0bee16418c6931596511aab
NormadjonGofforov <jumab@HP>
2023-02-27 23:18:26 +0500

    Updated

--- src/RegistanFerghanaLC.Service/Common/Attributes/PhoneNumberAttribute.cs
+++ src/RegistanFerghanaLC.Service/Common/Attributes/PhoneNumberAttribute.cs
@@ -1,5 +1,4 @@
-﻿using RegistanFerghanaLC.Service.Common.Validations;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace RegistanFerghanaLC.Service.Common.Attributes;
