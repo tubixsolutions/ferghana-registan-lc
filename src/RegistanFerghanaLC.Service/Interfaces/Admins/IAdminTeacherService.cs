e7746c29c5f408b3048177b1ebc913f9c6f06e35
Zulaykho Saparbaeva <saparbaevazulaykho18@gmail.com>
2023-02-27 15:38:49 +0500

    AdminService has been dvided into teacher & student service

---
+++ src/RegistanFerghanaLC.Service/Interfaces/Admins/IAdminTeacherService.cs
@@ -0,0 +1,8 @@
+﻿using RegistanFerghanaLC.Service.Dtos.Teachers;
+
+namespace RegistanFerghanaLC.Service.Interfaces.Admins;
+public interface IAdminTeacherService
+{
+    public Task<bool> RegisterTeacherAsync(TeacherRegisterDto teacherRegisterDto);
+
+}
