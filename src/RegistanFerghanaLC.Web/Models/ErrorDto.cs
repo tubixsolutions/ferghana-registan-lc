bd4688114728f356e4734750c077b7f6e671aabf
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2023-02-27 18:07:03 +0500

    middleware

---
+++ src/RegistanFerghanaLC.Web/Models/ErrorDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RegistanFerghanaLC.Web.Models
+{
+    public class ErrorDto
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
