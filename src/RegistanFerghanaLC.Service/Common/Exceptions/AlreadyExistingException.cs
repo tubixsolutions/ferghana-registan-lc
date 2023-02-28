afb23bd08860a9e22baf7be12483e56a91c8f68d
NormadjonGofforov <jumab@HP>
2023-02-28 23:22:29 +0500

    Updated

--- src/RegistanFerghanaLC.Service/Common/Exceptions/AlreadyExistingException.cs
+++ src/RegistanFerghanaLC.Service/Common/Exceptions/AlreadyExistingException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RegistanFerghanaLC.Service.Common.Exceptions
+﻿namespace RegistanFerghanaLC.Service.Common.Exceptions
 {
     public class AlreadyExistingException: Exception
     {
