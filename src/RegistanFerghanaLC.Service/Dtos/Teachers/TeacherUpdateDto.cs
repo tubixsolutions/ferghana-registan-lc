feb14dc2fd803dd97c00e2c718fea2cfce694a6e
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2023-03-02 13:52:05 +0500

    update controller

---
+++ src/RegistanFerghanaLC.Service/Dtos/Teachers/TeacherUpdateDto.cs
@@ -0,0 +1,25 @@
+﻿using RegistanFerghanaLC.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistanFerghanaLC.Service.Dtos.Teachers
+{
+    public class TeacherUpdateDto
+    {
+        public string FirstName { get; set; } = String.Empty;
+
+        public string LastName { get; set; } = String.Empty;
+
+        public string? Image { get; set; }
+
+        public string PhoneNumber { get; set; } = String.Empty;
+
+        public DateTime BirthDate { get; set; }
+
+        public string Subject { get; set; } = String.Empty;
+
+    }
+}
