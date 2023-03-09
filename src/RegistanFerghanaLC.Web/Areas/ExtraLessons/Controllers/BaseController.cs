711f438d4198599d327dcae8a21840152d2f1164
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2023-03-09 13:44:46 +0500

    extralesson

---
+++ src/RegistanFerghanaLC.Web/Areas/ExtraLessons/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RegistanFerghanaLC.Web.Areas.ExtraLessons.Controllers
+{
+    [Route("extraLessons")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public BaseController()
+        {
+
+        }
+    }
+}
