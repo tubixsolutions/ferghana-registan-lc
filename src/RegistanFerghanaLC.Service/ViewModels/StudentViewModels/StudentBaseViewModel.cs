101f89cecd6eede0d3d0177375a904db41e4fa2f
Zulaykho Saparbaeva <saparbaevazulaykho18@gmail.com>
2023-03-12 13:14:10 +0500

    minor

--- src/RegistanFerghanaLC.Service/ViewModels/StudentViewModels/StudentBaseViewModel.cs
+++ src/RegistanFerghanaLC.Service/ViewModels/StudentViewModels/StudentBaseViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace RegistanFerghanaLC.Service.ViewModels.StudentViewModels;
+﻿using RegistanFerghanaLC.Domain.Entities;
+
+namespace RegistanFerghanaLC.Service.ViewModels.StudentViewModels;
 public class StudentBaseViewModel
 {
     public int Id { get; set; }
@@ -8,6 +10,7 @@ public class StudentBaseViewModel
     public string LastName { get; set; } = String.Empty;
     
     public string PhoneNumber { get; set; } = String.Empty;
-    
+    public byte WeeklyLimit { get; set; }
+    public Subject Subjects { get; set; }
     public string? Image { get; set; }
 }
