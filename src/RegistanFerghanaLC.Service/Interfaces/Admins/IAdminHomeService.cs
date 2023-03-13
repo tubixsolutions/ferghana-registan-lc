63893deb21480e96c63c046d2171d3f3a7c0e098
Qahhor <nurmamatovnodirbek43@gmail.com>
2023-03-13 18:24:33 +0500

    Add ci/cd

---
+++ src/RegistanFerghanaLC.Service/Interfaces/Admins/IAdminHomeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistanFerghanaLC.Service.Interfaces.Admins
+{
+    public interface IAdminHomeService
+    {
+    }
+}
---
+++ src/RegistanFerghanaLC.Service/Services/AdminService/AdminHomeService.cs
@@ -0,0 +1,13 @@
+﻿using RegistanFerghanaLC.Service.Interfaces.Admins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistanFerghanaLC.Service.Services.AdminService
+{
+    public class AdminHomeService : IAdminHomeService
+    {
+    }
+}
