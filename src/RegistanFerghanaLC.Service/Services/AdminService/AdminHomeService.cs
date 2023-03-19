29d0934c632276d15f74cb04608d696d990e6c62
NormadjonGofforov <jumab@HP>
2023-03-19 16:59:29 +0500

    Updated

--- src/RegistanFerghanaLC.Service/Services/AdminService/AdminHomeService.cs
+++ src/RegistanFerghanaLC.Service/Services/AdminService/AdminHomeService.cs
@@ -1,9 +1,4 @@
 ﻿using RegistanFerghanaLC.Service.Interfaces.Admins;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RegistanFerghanaLC.Service.Services.AdminService
 {
